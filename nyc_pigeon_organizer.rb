a35654c93f17ad5d911bb1fd3ea29cb7efa6e629
Barak Rosner <rosnerbb@gmail.com>
2021-02-15 02:26:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   hash.each_with_object ({}) do |(key, value), final_array|
-    a[k] = v + 3
+    
   end 
   binding.pry 
 end
